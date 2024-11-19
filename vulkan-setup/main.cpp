388168927cbb5bcfe531cba1e9d1fd322530da4b
cam7minus1 <74940496+cam7minus1@users.noreply.github.com>
2024-11-19 11:13:48 -0500

    Added vulkan and other dependencies. DOES NOT RUN!
    Code updates

---
+++ vulkan-setup/main.cpp
@@ -0,0 +1,73 @@
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+using namespace std;
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    GLFWwindow* window;
+
+    void initWindow() {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void initVulkan() {
+
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+
+    }
+
+    void cleanup() {
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
+};
+
+/**
+ * @brief The main entry point of the application.
+ *
+ * This function creates an instance of HelloTriangleApplication,
+ * runs it, and handles any exceptions that may occur during execution.
+ *
+ * @return int Returns EXIT_SUCCESS if the application runs successfully,
+ *             or EXIT_FAILURE if an exception is caught.
+ */
+int main() {
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
